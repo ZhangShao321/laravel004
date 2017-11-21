242a63eba6fd8f74a6e260eec7e77e389bb922e8
limengyaoq123 <916173773@qq.com>
2017-11-21 22:19:01 +0800

    555

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,3 +15,42 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/*==============后台===============*/
+Route::get('/admin/login','admin\AdminLoginController@index'); 
+//执行登录的方法
+Route::post('/admin/dologin','admin\AdminLoginController@dologin');
+//生成登录验证码 
+Route::get('/admin/code','admin\AdminLoginController@code');
+        
+
+//后台路由组 中间件
+Route::group(['prefix'=>'admin','namespace'=>'admin','middleware'=>'adminlogin'], function () {
+
+		//进入后台的首页
+		Route::get('/index', 'CeshiController@index');
+
+		//后台user用户管理
+		Route::resource('/user','UserController');
+
+		//后台管理员管理
+		Route::resource('/guanliyuan','GuanliyuanController');
+
+		//后台商户(电影院)管理
+		Route::resource('/cinema','CinemaController');
+
+		//后台申请管理
+		Route::resource('/request','RequestController');
+
+		//后台影视分类
+		Route::resource('/film','FilmController');
+
+		//后台轮播图管理
+		Route::resource('/lunbo','LunboController');
+
+		//后台板块管理
+		Route::resource('/block','BlockController');
+
+		//后台网站配置
+		Route::resource('/net','NetController');
+
+});
