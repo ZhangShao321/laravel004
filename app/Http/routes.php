384e4e670162e1ca8841c04342ea63689f85cff4
peanuTnT <1810808055@qq.com>
2017-11-22 10:08:49 +0800

    1

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,3 +15,36 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+//===============================前台信息=============================
+
+
+
+
+Route::group(['prefix' => 'homes', 'namespace' => 'Homes'], function(){
+
+	//前台首页
+	Route::get('index','HomesController@index');
+
+	//电影列表页
+	Route::get('filmlist','HomesController@filmlist');
+
+	//电影详情页
+	Route::get('filmdetail','HomesController@filmdetail');
+
+	//电影院列表页
+	Route::get('cinemalist','HomesController@cinemalist');
+
+	//电影院详情
+    Route::get('cinemadetail','HomesController@cinemadetail');
+
+    //申请商户
+	Route::get('add','HomesController@add');
+    Route::post('store','HomesController@store');
+
+
+    //搜索框的页面
+    Route::get('search','HomesController@search');
+
+
+});
---
+++ resources/views/homes/search.blade.php
@@ -0,0 +1,84 @@
+@extends('homes.layout.moban')
+
+@section('title','电影列表页')
+
+@section('content')
+<!-- heading-banner start -->
+<div class="heading-banner">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<ul class="breadcrumb">
+					<li><a href="{{url('/homes/index')}}">首页</a></li>
+					<li class="active">搜索结果</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- heading-banner end -->
+
+<!-- content start -->
+<div class="shop-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+                <!-- 影片列表 start-->
+                <div class="shop-content">                   
+                    <div class="tab-content">
+                        <div role="tabpanel" class="tab-pane active" id="profile">
+                            <div class="row shop-list">
+                                @foreach($res as $k=>$v)
+                                <div class="col-md-12">
+                                    <div class="single-product">
+                                        <div class="product-img">
+                                            <a href="{{url('/homes/filmdetail?id=').$v->id}}">
+                                                <img class="primary-img" src="{{asset($v->filepic) }}" alt="" />
+                                                <img class="secondary-img" src="{{asset($v->filepic) }}" alt="" />
+                                            </a>
+                                        </div>
+                                        <div class="product-info">
+                                            <h3>
+                                                <a href="{{url('/homes/filmdetail?id=').$v->id}}">
+                                                    {{$v->filmname}}
+                                                </a>
+                                            </h3>
+                                            <div class="pro-price">
+                                                <span class="normal">
+                                                    导演:{{$v->director}}
+                                                </span>
+                                            </div>
+                                            <div class="pro-price">
+                                                <span class="normal">
+                                                    ￥{{$v->price}}
+                                                </span>
+                                            </div>
+                                            <div class="product-desc">
+                                                <p>
+                                                    {{$v->summary}}
+                                                </p>
+                                            </div>
+                                            <div class="product-action">
+                                                <div class="pro-button-top">
+                                                    <a href="{{url('/homes/filmdetail?id=').$v->id}}">
+                                                        详情
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                 <!-- 影片列表 end-->
+
+                
+            </div>
+        </div>
+    </div>
+</div>
+<!-- content end -->
+@endsection
